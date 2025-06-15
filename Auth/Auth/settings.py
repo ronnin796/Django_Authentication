46d9cf8d123829166de60024f4bc0fffa4a04f0f
ronnin796 <nischal.chaudhary796@gmail.com>
2025-06-15 21:23:43 +0545

    User Serializer Added

--- Auth/Auth/settings.py
+++ Auth/Auth/settings.py
@@ -125,6 +125,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # Media files (User-uploaded content)
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR , "media")
+AUTH_USER_MODEL = "users.CustomUser"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- Auth/api/users/models.py
+++ Auth/api/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=255,  null=True , default='Annonymous')
+    email = models.EmailField(unique=True, max_length=255, null=True)
+
+    username = None  # Disable username field
+    USERNAME_FIELD = 'email'  # Use email as the unique identifier
+    REQUIRED_FIELDS = []  # No required fields other than email
+    phone = models.CharField(max_length=15, null=True, blank=True)
+
+    gender = models.CharField(
+        max_length=10,
+    )
+    session_token = models.CharField(max_length=10 , default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+                            
